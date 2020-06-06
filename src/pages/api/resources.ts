3a2537f91796f9708f90e9c35fe4b6369a55724f
sridhar02 <kattasridhar02@gmail.com>
2020-06-06 20:47:24 +0530

    changing the getallResource function and related functions

--- src/pages/api/resources.ts
+++ src/pages/api/resources.ts
@@ -3,7 +3,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { getAllResources } from '../../utils/getAllResources'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const allResourcesResult = await getAllResources({})
+  const allResourcesResult = await getAllResources()
 
   if (allResourcesResult.error) {
     return res.status(500).json({ message: allResourcesResult.message })
